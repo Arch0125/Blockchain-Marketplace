2cfb9c69b37c8005dc5f2dab69f1863113088459
Arch0125 <dasarchisman25@gmail.com>
2022-02-16 23:08:05 +0530

    Product Creation contract added

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,36 @@ pragma solidity ^0.4.18;
 
 contract Marketplace{
     string public name;
+    uint public productCount = 0;
+
+    struct product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased; 
+    }
+
+    event ProductAdded{
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased; 
+    }
+
+    mapping(uint => product) public products;
 
     function Marketplace() public {
         name = "Blockchain Marketplace";
+    }
+
+    function addProduct(string memory _name, uint memory _price) public {
+        require(bytes(name).length > 0);
+        require(_price > 0);
+        productCount++;
+        products[productCount]=product(productCount,_name,_price,msg.sender,false);
+        emit ProductAdded(productCount,_name,_price,msg.sender,false);
+
     }
 }
