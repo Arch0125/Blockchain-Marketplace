9c03247dcf19027bb9b04cf8a0ad4a4277027ea0
Arch0125 <dasarchisman25@gmail.com>
2022-02-19 00:24:54 +0530

    web3 added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
+
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable()
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('Non web3 browser detected! Use MetaMask extension')
+    }
+  }
+
+
   render() {
     return (
       <div>
